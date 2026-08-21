0c48fafb1f1a6acb767913157711d5fb6aecdd69
nanirise <12088001@qq.com>
2026-08-21 21:18:50 +0800

    feat: 搭建 Gin 骨架，添加 /ping 接口

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 请来领班：创建一个 Gin 引擎（默认已带"日志记录"和"崩溃自动恢复"两个贴心服务）
+	r := gin.Default()
+
+	// 定好菜单：当顾客用 GET 方式请求 "/ping" 时，领班带他去这个窗口
+	r.GET("/ping", func(c *gin.Context) {
+		// c 是领班递给你的"顾客上下文"，装着这次请求的全部信息
+		// c.JSON(状态码, 数据)：领班自动把数据包装成 JSON 端给顾客
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// 正式营业：守在 8080 号门
+	r.Run(":8080")
+}
